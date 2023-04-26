4002ae9d9235e16dab4f96acdaa3c2eacc92f454
Tahoe Schrader <tahoeschrader@gmail.com>
2023-04-25 22:23:28 -0400

    Remove commented out code about sql table creation order now that we have migrations in place.
    This commented code as only there for the time being to help us remember the order in which we needed to generate the
    migrations files

--- backend/scripts/test.sh
+++ backend/scripts/test.sh
@@ -4,15 +4,3 @@ docker-compose up -d db
 export $(cat .env | xargs)
 sleep 10
 cargo test
-
-# Create model tables
-# psql -f migrations/models/1-enums.sql postgres
-# psql -f migrations/models/2-user.sql postgres
-# psql -f migrations/models/3-player.sql postgres
-# psql -f migrations/models/4-trophy.sql postgres
-# psql -f migrations/models/5-score_type.sql postgres
-# psql -f migrations/models/6-league.sql postgres
-# psql -f migrations/models/7-game.sql postgres
-# psql -f migrations/models/8-payment.sql postgres
-# psql -f migrations/models/9-score.sql postgres
-# psql -f migrations/models/10-attendance.sql postgres
