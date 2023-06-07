61f8c0e3e6b3eec626ad019e22ff67adfb8994ec
Tristan Schrader <tristan@climaxfoods.com>
2023-06-06 19:02:23 -0700

    move AppIOS into main.io.kt and rename to App

--- frontend/shared/src/iosMain/kotlin/main.ios.kt
+++ frontend/shared/src/iosMain/kotlin/main.ios.kt
@@ -3,10 +3,20 @@
 )
 
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeUIViewController
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 
 actual fun getPlatformName(): String = "iOS"
 
-fun MainViewController() = ComposeUIViewController { AppIOS() }
+@Composable
+actual fun App() {
+    Text("Hello World", modifier = Modifier.padding(16.dp))
+}
+
+fun MainViewController() = ComposeUIViewController { App() }
